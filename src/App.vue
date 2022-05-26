72860f326634528ac785b974987caa2d53d11f18
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-26 13:20:27 +0300

    Урок 2 - перенос исходных файлов стилей, изображений; создание компонентов: меню-навигация, футер, карточка товара; вывод данных компонентов на главной странце (компонент HeroView)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="app">
-    Hello Vue.js
-  </div>
+    <div class="wrapper">
+        <hero-view />
+        <footer-component />
+    </div>
 </template>
 
 <script>
-
+import HeroView from '@/views/HeroView.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
 export default {
-
+    components: { HeroView, FooterComponent },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
