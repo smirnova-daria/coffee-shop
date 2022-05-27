d16336f48372daeff0cc9b5cd1473f275c37fd4b
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-27 15:39:53 +0300

    Урок 2 - перенос исходных файлов стилей, изображений; создание компонентов: меню-навигация, футер, карточка товара; вывод данных компонентов на главной странце (компонент HeroView)

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -70,9 +70,9 @@
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1">
                         <div class="best__wrapper">
-                            <card-component />
-                            <card-component />
-                            <card-component />
+                            <product-card />
+                            <product-card />
+                            <product-card />
                         </div>
                     </div>
                 </div>
@@ -83,8 +83,8 @@
 
 <script>
 import NavBarComponent from '@/components/NavBarComponent.vue'
-import CardComponent from '@/components/CardComponent.vue'
+import ProductCard from '@/components/ProductCard.vue'
 export default {
-    components: { NavBarComponent, CardComponent },
+    components: { NavBarComponent, ProductCard },
 }
 </script>
