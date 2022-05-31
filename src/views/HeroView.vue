cc7913be142c34f6cb75024476c46a1deb940c26
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-31 13:42:35 +0300

    Урок 4: состояние компонента: подготовка данных для последующего вывода на страницах OurCoffee, ForYourPleasure; Динамический вывод данных из состояния для ссылок NavBar, Footer

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -88,24 +88,24 @@ export default {
     components: { NavBarComponent, ProductCard },
     data() {
         return {
-            cards: [
+            bestsellers: [
                 {
                     id: 0,
                     image: 'coffee-1.jpg',
-                    title: 'Solimo Coffee Beans 2kg',
-                    price: '10.73$',
+                    name: 'Solimo Coffee Beans 2kg',
+                    price: 10.73,
                 },
                 {
                     id: 1,
                     image: 'coffee-2.jpg',
-                    title: 'Presto Coffee Beans 1kg',
-                    price: '15.99$',
+                    name: 'Presto Coffee Beans 1kg',
+                    price: 15.99,
                 },
                 {
                     id: 2,
                     image: 'coffee-3.jpg',
-                    title: 'AROMISTICO Coffee 1kg',
-                    price: '6.99$',
+                    name: 'AROMISTICO Coffee 1kg',
+                    price: 6.99,
                 },
             ],
         }
--- src/views/OurCoffeeView.vue
+++ src/views/OurCoffeeView.vue
@@ -94,48 +94,48 @@ export default {
     components: { NavBarComponent, ProductCard },
     data() {
         return {
-            cards: [
+            coffee: [
                 {
                     id: 0,
                     image: 'coffee-1.jpg',
-                    title: 'Solimo Coffee Beans 2kg',
+                    name: 'Solimo Coffee Beans 2kg',
                     country: 'Brazil',
-                    price: '10.73$',
+                    price: 10.73,
                 },
                 {
                     id: 1,
                     image: 'coffee-2.jpg',
-                    title: 'Presto Coffee Beans 1kg',
+                    name: 'Presto Coffee Beans 1kg',
                     country: 'Ethiopia',
-                    price: '15.99$',
+                    price: 15.99,
                 },
                 {
                     id: 2,
                     image: 'coffee-3.jpg',
-                    title: 'AROMISTICO Coffee 1kg',
+                    name: 'AROMISTICO Coffee 1kg',
                     country: 'Colombia',
-                    price: '6.99$',
+                    price: 6.99,
                 },
                 {
                     id: 3,
                     image: 'coffee-1.jpg',
-                    title: 'Solimo Coffee Beans 2kg',
+                    name: 'Solimo Coffee Beans 2kg',
                     country: 'Guatemala',
-                    price: '10.73$',
+                    price: 10.73,
                 },
                 {
                     id: 4,
                     image: 'coffee-2.jpg',
-                    title: 'Presto Coffee Beans 1kg',
+                    name: 'Presto Coffee Beans 1kg',
                     country: 'India',
-                    price: '15.99$',
+                    price: 15.99,
                 },
                 {
                     id: 5,
                     image: 'coffee-3.jpg',
-                    title: 'AROMISTICO Coffee 1kg',
+                    name: 'AROMISTICO Coffee 1kg',
                     country: 'Tanzania',
-                    price: '6.99$',
+                    price: 6.99,
                 },
             ],
         }
