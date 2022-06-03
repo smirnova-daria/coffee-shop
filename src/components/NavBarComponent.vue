3d00b3d2c5abbd73bc424ed33a39e8b21120268d
Daria Smirnova <darya.smirn1@yandex.ru>
2022-06-03 14:39:25 +0300

    Урок 8: работа с Vuex: перенос данных из компонентов представления в отдельные модули store: ссылки для NavBar и Footer, карточки товаров для Hero, ForYourPleasure, OurCoffee

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -3,7 +3,6 @@
         <ul
             class="header d-flex justify-content-center justify-content-md-start flex-wrap"
         >
-            
             <nav-item :link="links.header.link" classItem="header__item">
                 <img
                     :src="require(`@/assets/logo/${links.header.icon}`)"
@@ -26,33 +25,10 @@
 import NavItem from './NavItem.vue'
 export default {
     components: { NavItem },
-    data() {
-        return {
-            links: {
-                header: {
-                    id: 0,
-                    link: '/',
-                    icon: 'Logo.svg',
-                },
-                other: [
-                    {
-                        id: 1,
-                        text: 'Our coffee',
-                        link: '/our-coffee',
-                    },
-                    {
-                        id: 2,
-                        text: 'For your pleasure',
-                        link: '/pleasure',
-                    },
-                    {
-                        id: 3,
-                        text: 'Contact us',
-                        link: '/contacts',
-                    },
-                ],
-            },
-        }
+    computed: {
+        links() {
+            return this.$store.getters['getHeaderLinks']
+        },
     },
 }
 </script>
--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -92,33 +92,13 @@
 import NavBarComponent from '@/components/NavBarComponent.vue'
 import ProductCard from '@/components/ProductCard.vue'
 import HeaderTitleComponent from '@/components/HeaderTitleComponent.vue'
-import { v4 as uuidv4 } from 'uuid'
+
 import { scrollIntoView } from 'seamless-scroll-polyfill'
 export default {
     components: { NavBarComponent, ProductCard, HeaderTitleComponent },
     data() {
         return {
             headerTitle: 'Everything You Love About Coffee',
-            bestsellers: [
-                {
-                    id: uuidv4(),
-                    image: 'coffee-1.jpg',
-                    name: 'Solimo Coffee Beans 2kg',
-                    price: 10.73,
-                },
-                {
-                    id: uuidv4(),
-                    image: 'coffee-2.jpg',
-                    name: 'Presto Coffee Beans 1kg',
-                    price: 15.99,
-                },
-                {
-                    id: uuidv4(),
-                    image: 'coffee-3.jpg',
-                    name: 'AROMISTICO Coffee 1kg',
-                    price: 6.99,
-                },
-            ],
         }
     },
     methods: {
@@ -127,6 +107,11 @@ export default {
                 behavior: 'smooth',
                 block: 'start',
             })
+        },
+    },
+    computed: {
+        bestsellers() {
+            return this.$store.getters['getBestsellers']
         },
     },
 }
