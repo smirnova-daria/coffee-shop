5dabb51d6e4d5e68aa40374e20336c20774c5e87
Daria Smirnova <darya.smirn1@yandex.ru>
2022-06-09 11:36:32 +0300

    Урок 11: работа с json-server: получение данных из db.json, сохранение данных из формы

--- src/store/bestsellers.js
+++ src/store/bestsellers.js
@@ -1,26 +1,6 @@
 const bestsellers = {
     state: {
-        bestsellers: null,
-        // bestsellers: [
-        //     {
-        //         id: 0,
-        //         image: 'coffee-1.jpg',
-        //         name: 'Solimo Coffee Beans 2kg',
-        //         price: 10.73,
-        //     },
-        //     {
-        //         id: 1,
-        //         image: 'coffee-2.jpg',
-        //         name: 'Presto Coffee Beans 1kg',
-        //         price: 15.99,
-        //     },
-        //     {
-        //         id: 2,
-        //         image: 'coffee-3.jpg',
-        //         name: 'AROMISTICO Coffee 1kg',
-        //         price: 6.99,
-        //     },
-        // ],
+        bestsellers: [],
     },
     actions: {
         setBestsellersData({ commit }, data) {
